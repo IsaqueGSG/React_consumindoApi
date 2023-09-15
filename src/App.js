71b3509c8426d1240f080ffd3ed33a096c92bab5
unknown <isaquegsgoncalves@gmail.com>
2023-09-15 13:05:27 -0300

    adc inputs and scan quagga

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { 
+  styled,
+  Box, 
+  TextField, 
+  Button, 
+  Grid, 
+  Container,
+  CssBaseline,
+  Typography,
+  MenuItem,
+  Select,
+  InputLabel,
+  FormControl,
+} from "@mui/material";
+
+import Scanner from "./components/scan";
 
 function App() {
-  let [code, setcode] = useState('');
+  let [Nserie, setNserie] = useState('');
   let [patrimonio, setpatrimonio] = useState('');
+  let [local, setLocal] = useState('');
+  let [inventariante, setInventariante] = useState('');
+  let [obs, setObs] = useState('');
+  let [statusBem, setStatusBem] = useState('');
+  let [tipoBem, setTipoBem] = useState('');
+
+  let [camera, setCamera] = useState(false);
+  let [result, setResult] = useState(null);
+
+  const onDetected = result => {
+    setResult(result);
+  };
 
   async function handleSubmit(e){
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3001/api',{
-        code,
+      const response = await axios.post('http://localhost:3001/',{
+        Nserie,
         patrimonio
       },
       {
@@ -23,20 +51,207 @@ function App() {
       console.error(error);
     }
   }
+  const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
 
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        Code:
-        <input type="text" name="code" onChange={(e)=> setcode(e.target.value)}></input>
+    <>
+
+    
+      <div className="container">
+        {camera && <Scanner onDetected={onDetected} />}
       </div>
-      <div>
-        patrimonio:
-        <input type="text" name="patrimonio" onChange={(e)=> setpatrimonio(e.target.value)}></input>
-      </div>
-      <input type="submit"></input>
-    </form>
+    
+    <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Mapeamento de Bens
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+                           
+              <Grid item xs={12} sm={6}>
+
+                <FormControl sx={{ width: 1 }}>
+                  <InputLabel id="labelInventariante">INVENTARIANTE</InputLabel>
+                  <Select
+                    labelId="labelInventariante"
+                    label='labelInventariante'
+                    id="inventariante"
+                    value={inventariante}
+                    onChange={(e)=> setInventariante(e.target.value)}
+                  >
+                    <MenuItem value={'isaque gabriel da silva goncalves'}>isaque</MenuItem>
+                  <MenuItem value={'thais ...'}>Thais</MenuItem>
+                  <MenuItem value={'gleidson juan ..'}>Juan</MenuItem>
+                  </Select>
+                </FormControl>
+              
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+
+                <FormControl sx={{ width: 1 }}>
+                  <InputLabel id="labelLocal">LOCAL</InputLabel>
+                  <Select
+                    labelId="labelLocal"
+                    label="labelLocal"
+                    id="local"
+                    value={local}
+                    onChange={(e)=> setLocal(e.target.value)}
+                  >
+                  <MenuItem value={320}>Sala 320</MenuItem>
+                  <MenuItem value={206}>Sala 206</MenuItem>
+                  <MenuItem value={307}>Sala 307</MenuItem>
+                  </Select>
+                </FormControl>
+
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+
+                <FormControl sx={{ width: 1 }}>
+                  <InputLabel id="labelStatus">STATUS</InputLabel>
+                  <Select
+                    labelId="labelStatus"
+                    label="labelStatus"
+                    id="statusBem"
+                    value={statusBem}
+                    onChange={(e)=> setStatusBem(e.target.value)}
+                  >
+                   <MenuItem value={'ocioso'}>Ocioso</MenuItem>
+                  <MenuItem value={'reparo'}>Necessita de reparo</MenuItem>
+                  <MenuItem value={"Uso"}>Em uso</MenuItem>
+                  </Select>
+                </FormControl>
+
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+
+                <FormControl sx={{ width: 1 }}>
+                  <InputLabel id="labelTipo">TIPO</InputLabel>
+                  <Select
+                    labelId="labelTipo"
+                    label="labelTipo"
+                    id="tipo"
+                    value={tipoBem}
+                    onChange={(e)=> setTipoBem(e.target.value)}
+                  >
+                    <MenuItem value={'cadeira'}>Cadeira</MenuItem>
+                  <MenuItem value={'cpu'}>Cpu</MenuItem>
+                  <MenuItem value={"monitor"}>Monitor</MenuItem>
+                  </Select>
+                </FormControl>
+              
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  name="patrimonio"
+                  fullWidth
+                  id="patrimonio"
+                  label="Patrimonio"
+                  autoFocus
+                  value={ result ? result : patrimonio }
+                  onChange={(e)=> setpatrimonio(e.target.value)}
+                  InputProps={{
+                    endAdornment: 
+                    <Button variant="outlined" onClick={() => setCamera(!camera)}>
+                      {camera ? "Stop" : "Ler"}
+                    </Button>}
+                  }
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  id="Nserie"
+                  label="Numero de Serie"
+                  name="Nserie"
+                  value={ result ? result : Nserie }
+                  onChange={(e)=> setNserie(e.target.value)}
+                  InputProps={{
+                    endAdornment: 
+                    <Button variant="outlined" onClick={() => setCamera(!camera)}>
+                      {camera ? "Stop" : "Ler"}
+                      
+                    </Button>}
+                  }
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  name="observacao"
+                  fullWidth
+                  id="observacao"
+                  label="Observação"
+                  autoFocus
+                  value={obs}
+                  onChange={(e)=> setObs(e.target.value)}
+                />
+              </Grid>
+
+              <Grid item xs={6}>
+                <Button component="label" variant="contained" sx={{ width: 1 }}>
+                  FOTO 1
+                  <VisuallyHiddenInput type="file" />
+                </Button>
+              </Grid>
+
+              <Grid item xs={6}>
+                <Button component="label" variant="contained" sx={{ width: 1 }}>
+                  FOTO 2
+                  <VisuallyHiddenInput type="file" />
+                </Button>
+              </Grid>
+
+              <Grid item xs={6}>
+                <Button component="label" variant="contained" sx={{ width: 1 }}>
+                  FOTO 3
+                  <VisuallyHiddenInput type="file" />
+                </Button>
+              </Grid>
+              
+        
+              <Grid item xs={6}>
+                <Button component="label" variant="contained" sx={{ width: 1 }}>
+                  FOTO 4
+                  <VisuallyHiddenInput type="file" />
+                </Button>
+              </Grid>             
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </>
   );
 }
 
